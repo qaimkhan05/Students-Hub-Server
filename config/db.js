956fbbbc4ab85ae47860cd46d7d71b36f8cfa5fb
qaimkhan05 <your_noreply_email_here>
2026-08-04 00:17:43 +0500

    Fix MongoDB SRV DNS resolution by using public DNS servers

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+mongoose.connection.on('error', (err) => {
+  console.error(`MongoDB Connection Error: ${err.message}`);
+});
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
